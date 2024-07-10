149afd6eac3eaa00efe05e70b692e020a0c9a655
Prashantjha14 <129178920+Prashantjha14@users.noreply.github.com>
2024-07-10 21:26:39 +0530

    fix mobile blog button

--- src/components/navbar/MobileNav.tsx
+++ src/components/navbar/MobileNav.tsx
@@ -18,7 +18,9 @@ const MobileNav = () => {
       </div>
       <button className="group relative scale-100 overflow-hidden rounded-lg px-4 py-2 transition-transform hover:scale-105 active:scale-95">
         <span className="relative z-10 text-white/90 transition-colors group-hover:text-white">
-          Blog.
+          <Link href="https://prashantjha14.hashnode.dev/" target="_blank">
+            Blog.
+          </Link>
         </span>
         <span className="absolute inset-0 z-0 bg-gradient-to-br from-white/20 to-white/5 opacity-0 transition-opacity group-hover:opacity-100"></span>
       </button>
